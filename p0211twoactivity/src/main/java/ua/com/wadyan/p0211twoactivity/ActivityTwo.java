7f50fc1c1fb58656d105d3c60c06785e5554f747
Wad + <wadyan.r@gmail.com>
2015-11-17 07:14:08 +0200

    lesson 21 twoactivity

---
+++ p0211twoactivity/src/main/java/ua/com/wadyan/p0211twoactivity/ActivityTwo.java
@@ -0,0 +1,12 @@
+package ua.com.wadyan.p0211twoactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTwo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
